4a719fbd8e1a8c1b54f9abb998d5aa46d2c885d8
yurisxzt <silva.yuri@escolar.ifrn.edu.br>
2026-04-14 10:23:53 -0300

    Mais ajustes

--- 3Drepositorio/Assets/Script/BootLoader.cs
+++ 3Drepositorio/Assets/Script/BootLoader.cs
@@ -1,54 +1,9 @@
-﻿using System;
-using System.IO;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-namespace Script
+public class BootLoader : MonoBehaviour
 {
-    public class BootLoader : MonoBehaviour
+    private void Start()
     {
-        private const string BootSceneName = "_Boot";
-
-        private void Start()
-        {
-            // If this is not running in the boot scene, do nothing
-            if (SceneManager.GetActiveScene().name != BootSceneName) return;
-
-            // Try to find the additive scene that isn't _Boot
-            for (int i = 0; i < SceneManager.sceneCount; i++)
-            {
-                var s = SceneManager.GetSceneAt(i);
-                if (!s.IsValid()) continue;
-                if (s.name == BootSceneName) continue;
-
-                // make the other scene active and unload boot
-                SceneManager.SetActiveScene(s);
-                SceneManager.UnloadSceneAsync(BootSceneName);
-                return;
-            }
-
-            // fallback: try to read temp file created by the editor
-            try
-            {
-                var projectPath = Path.GetFullPath(Path.Combine(Application.dataPath, ".."));
-                var fullPath = Path.Combine(projectPath, "Temp", "boot_target_scene.txt");
-                if (File.Exists(fullPath))
-                {
-                    var scenePath = File.ReadAllText(fullPath).Trim();
-                    if (!string.IsNullOrEmpty(scenePath))
-                    {
-                        var sceneName = Path.GetFileNameWithoutExtension(scenePath);
-                        SceneManager.LoadScene(sceneName);
-                        SceneManager.UnloadSceneAsync(BootSceneName);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.LogError($"BootLoader: Failed to load target scene from temp file: {e}");
-            }
-        }
+        GameManager.Instance.LoadScene("Splash");
     }
 }
-
-
